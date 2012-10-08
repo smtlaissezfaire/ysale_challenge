8bc5dea27625e1c05af9d047f39ee8e9059dac8e
Scott Taylor <scott@railsnewbie.com>
2012-10-07 22:22:15 -0700

    hacky version to move multiple tiles at once.

--- YardsalePuzzle/ViewController.m
+++ YardsalePuzzle/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 
 #define TILES_PER_ROW 4
-#define TILE_COUNT 15
+#define ROWS 4
 
 @interface ViewController () {
     NSMutableArray *buttons_;
@@ -21,81 +21,125 @@
 
 - (void)viewDidLoad
 {
-    buttons_ = [NSMutableArray array];
-
     [super viewDidLoad];
     [self layoutTiles];
 }
 
 - (void) layoutTiles {
-    CGFloat xOffset = 0;
+    UIImage *largeImage = [UIImage imageNamed: @"yardsale.jpeg"];
     CGFloat frameWidth = self.view.frame.size.width;
     CGFloat frameHeight = self.view.frame.size.height;
     CGFloat tileWidth = frameWidth / TILES_PER_ROW;
-    CGFloat tileHeight = frameHeight / TILES_PER_ROW;
-    CGFloat yOffset = -tileHeight;
+    CGFloat tileHeight = frameHeight / ROWS;
+    CGFloat xOffset = 0;
+    CGFloat yOffset = 0;
 
-    UIImage *largeImage = [UIImage imageNamed: @"yardsale.jpeg"];
+    // 4x4 array
+    buttons_ = [NSMutableArray arrayWithCapacity: ROWS];
 
-    for (int i = 0; i < TILE_COUNT; i++) {
-        if ((i % TILES_PER_ROW) == 0) {
-            xOffset = 0;
-            yOffset += tileHeight;
-        } else {
+    for (int i = 0; i < ROWS; i++) {
+        NSMutableArray *array = [NSMutableArray arrayWithCapacity: TILES_PER_ROW];
+        [buttons_ addObject: array];
+
+        for (int j = 0; j < TILES_PER_ROW; j++) {
+            if (i == ROWS - 1 && j == TILES_PER_ROW - 1) {
+                [array addObject: [NSNull null]];
+                continue;
+            }
+
+            UIButton *button = [UIButton buttonWithType: UIButtonTypeRoundedRect];
+            button.frame = CGRectMake(xOffset, yOffset, tileWidth, tileHeight);
+
+            CGImageRef imageRef = CGImageCreateWithImageInRect([largeImage CGImage], button.frame);
+            UIImage *image = [UIImage imageWithCGImage:imageRef];
+            CGImageRelease(imageRef);
+
+            [button setImage: image forState:UIControlStateNormal];
+            [button addTarget:self action:@selector(selectButton:) forControlEvents:UIControlEventTouchUpInside];
+            [self.view addSubview: button];
+
+            [array addObject: button];
+
             xOffset += tileWidth;
         }
 
-        UIButton *button = [UIButton buttonWithType: UIButtonTypeRoundedRect];
-        button.frame = CGRectMake(xOffset, yOffset, tileWidth, tileHeight);
-
-        CGImageRef imageRef = CGImageCreateWithImageInRect([largeImage CGImage], button.frame);
-        UIImage *image = [UIImage imageWithCGImage:imageRef];
-        CGImageRelease(imageRef);
-
-        [button setImage: image forState:UIControlStateNormal];
-
-        [buttons_ addObject: button];
-
-        [button addTarget:self action:@selector(selectButton:) forControlEvents:UIControlEventTouchUpInside];
-        [self.view addSubview: button];
+        xOffset = 0;
+        yOffset += tileHeight;
     }
-
-    [buttons_ addObject: [NSNull null]];
 }
 
 - (void) selectButton: (UIButton *) button {
-    int buttonIndex = [buttons_ indexOfObject: button];
-    int emptyIndex = [buttons_ indexOfObject: [NSNull null]];
+    int emptyXIndex = 0;
+    int emptyYIndex = 0;
+    int buttonXIndex = 0;
+    int buttonYIndex = 0;
+
+    // find the empty one
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < TILES_PER_ROW; j++) {
+            UIButton *b = [[buttons_ objectAtIndex: i] objectAtIndex: j];
+
+            if ((NSNull *) b == [NSNull null]) {
+                emptyXIndex = i;
+                emptyYIndex = j;
+            } else if (b == button) {
+                buttonXIndex = i;
+                buttonYIndex = j;
+            }
+        }
+    }
 
     CGFloat buttonWidth = button.frame.size.width;
     CGFloat buttonHeight = button.frame.size.height;
-    CGFloat targetXOffset = (emptyIndex % TILES_PER_ROW) * buttonWidth;
-    CGFloat targetYOffset = (emptyIndex / TILES_PER_ROW) * buttonHeight;
-    CGFloat buttonXOffset = (buttonIndex % TILES_PER_ROW) * buttonWidth;
-    CGFloat buttonYOffset = (buttonIndex / TILES_PER_ROW) * buttonHeight;
 
-    // if it doesn't border it on one side, we can't do anything
-    if (targetYOffset == buttonYOffset) {
-        if (buttonXOffset + buttonWidth != targetXOffset &&
-            buttonXOffset - buttonWidth != targetXOffset &&
-            buttonXOffset != targetXOffset) {
-                return;
+    if (buttonXIndex == emptyXIndex) { // right / left shifts
+        NSMutableArray *row = [buttons_ objectAtIndex: buttonXIndex];
+        if (emptyYIndex > buttonYIndex) { // right shift
+            for (int i = emptyYIndex; i > buttonYIndex; i--) {
+                UIButton *b = [row objectAtIndex: i - 1];
+
+                [UIView animateWithDuration:1.0 animations: ^{
+                    b.frame = CGRectMake(b.frame.origin.x + buttonWidth, b.frame.origin.y, buttonWidth, buttonHeight);
+                }];
+
+                [row exchangeObjectAtIndex: i withObjectAtIndex: i-1];
+            }
+        } else { // left shift
+            for (int i = emptyYIndex; i < buttonYIndex; i++) {
+                UIButton *b = [row objectAtIndex: i + 1];
+
+                [UIView animateWithDuration:1.0 animations: ^{
+                    b.frame = CGRectMake(b.frame.origin.x - buttonWidth, b.frame.origin.y, buttonWidth, buttonHeight);
+                }];
+
+                [row exchangeObjectAtIndex: i withObjectAtIndex: i+1];
+            }
         }
-    } else if (targetXOffset == buttonXOffset) {
-        if (buttonYOffset + buttonHeight != targetYOffset &&
-            buttonYOffset - buttonHeight != targetYOffset &&
-            buttonYOffset != targetYOffset) {
-                return;
+    } else if (buttonYIndex == emptyYIndex) { // up / down shifts
+        if (emptyXIndex > buttonXIndex) { // down
+            for (int i = emptyXIndex; i > buttonXIndex; i--) {
+                UIButton *b = [[buttons_ objectAtIndex: i - 1] objectAtIndex: buttonYIndex];
+
+                [UIView animateWithDuration:1.0 animations: ^{
+                    b.frame = CGRectMake(b.frame.origin.x, b.frame.origin.y + buttonHeight, buttonWidth, buttonHeight);
+                }];
+
+                [[buttons_ objectAtIndex: i] replaceObjectAtIndex: buttonYIndex withObject: b];
+                [[buttons_ objectAtIndex: i - 1] replaceObjectAtIndex: buttonYIndex withObject: [NSNull null]];
+            }
+        } else {
+            for (int i = emptyXIndex; i < buttonXIndex; i++) { // up
+                UIButton *b = [[buttons_ objectAtIndex: i + 1] objectAtIndex: buttonYIndex];
+
+                [UIView animateWithDuration:1.0 animations: ^{
+                    b.frame = CGRectMake(b.frame.origin.x, b.frame.origin.y - buttonHeight, buttonWidth, buttonHeight);
+                }];
+
+                [[buttons_ objectAtIndex: i] replaceObjectAtIndex: buttonYIndex withObject: b];
+                [[buttons_ objectAtIndex: i + 1] replaceObjectAtIndex: buttonYIndex withObject: [NSNull null]];
+            }
         }
-    } else {
-        return;
     }
-
-    [buttons_ exchangeObjectAtIndex: buttonIndex withObjectAtIndex: emptyIndex];
-
-    [UIView animateWithDuration:1.0 animations: ^{
-        button.frame = CGRectMake(targetXOffset, targetYOffset, buttonWidth, buttonHeight);
-    }];
 }
 
 - (void)viewDidUnload
